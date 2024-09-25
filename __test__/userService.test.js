76b6f32b617fea32f3cabb38746d9ba3990c47a0
Ian-Hoole <hoolei@sonoma.edu>
2024-09-24 17:32:50 -0700

    Added user service test cases

--- __test__/userService.test.js
+++ __test__/userService.test.js
@@ -1,30 +1,178 @@
 const userService = require("../src/service/userService.js");
-const userDao = require("../repository/userDAO.js");
-const { v4: uuidv4 } = require('uuid');
+const userDao = require("../src/repository/userDAO.js");
 const bcrypt = require("bcrypt");
+//const logger = require("../src/util/logger.js");
 
-jest.mock("userDao");
-jest.mock("bcrypt");
-jest.mock("uuidv4");
+//Mock Database & reset function
+let mockUserDB = [];
+rebuildDatabase();
 
+function rebuildDatabase() {
+    mockUserDB = [{ user_id: "1", username: "alex", password: "1234", role: "employee" },
+    { user_id: "2", username: "administrator", password: "45678", role: "manager" },
+    { user_id: "3", username: "phil", password: "dang", role: "employee" }];
+}
 
+//Mocked functions
+userDao.createUser = jest.fn(async (user) => {
+    await mockUserDB.push(user);
+    return 200;
+});
+userDao.getUserByUsername = jest.fn(async (username) => {
+    let tempUser = null;
+    await mockUserDB.forEach(user => {
+        if(user.username === username){
+            tempUser = user;
+        }
+    });
+    return tempUser;
+});
+bcrypt.hash = jest.fn(async (str) => {
+    return str;
+});
+bcrypt.compare = jest.fn(async (str1, str2) => {
+    return str1 === str2;
+});
+jest.mock('uuid', () => ({ v4: () => `${mockUserDB.length + 1}` }));
+//logger.info = jest.fn((str) => { console.log("info: " + str)});
+//logger.error = jest.fn((str) => { console.log("error: " + str) });
 
-const mockUserDB = [{ user_id: "1", username: "alex", password: "1234", role: "employee" }, 
-                    { user_id: "2", username: "administrator", password: "45678", role: "manager" },
-                    { user_id: "3", username: "phil", password: "dang", role: "employee" }];
+//Test cases
+describe("Testing user creation via userService.createUser", () => {
+    beforeEach(() => {
+        rebuildDatabase();
+        userDao.createUser.mockClear();
+        userDao.getUserByUsername.mockClear();
+    });
+    test("Adding a user without password parameters", async () => {
+        //Arrange
+        const username = "john";
+        const password = "";
 
+        let result = null
+        //Action
+        result = await userService.createUser(username, password);
+        //Assert
+        expect(result).toBeNull();
+        expect(userDao.getUserByUsername).not.toHaveBeenCalled();
+        expect(userDao.createUser).not.toHaveBeenCalled();
+    });
+    test("Adding a new user without a username", async () => {
+        //Arrange
+        const username = "";
+        const password = "pass";
 
-describe("Testing user ", () => {
-    test("Sample Test", () => {
-        //AAA
+        let result = null
+        //Action
+        result = await userService.createUser(username, password);
+        //Assert
+        expect(result).toBeNull();
+        expect(userDao.getUserByUsername).not.toHaveBeenCalled();
+        expect(userDao.createUser).not.toHaveBeenCalled();
+    });
+    test("Adding a duplicate user", async () => {
+        //Arrange
+        const username = "alex";
+        const password = "pass";
+        const dbResult = [...mockUserDB];
+        let result = null
+        //Action
+        result = await userService.createUser(username, password);
+        //Assert
+        expect(result).toBeNull();
+        expect(userDao.getUserByUsername).toHaveBeenCalled();
+        expect(userDao.createUser).not.toHaveBeenCalled();
+        expect(mockUserDB).toEqual(dbResult);
+    });
+    test("Adding a new valid user", async () => {
+        //Arrange
+        const username = "john";
+        const password = "pass";
 
+        const dbResult = [{ user_id: "1", username: "alex", password: "1234", role: "employee" },
+        { user_id: "2", username: "administrator", password: "45678", role: "manager" },
+        { user_id: "3", username: "phil", password: "dang", role: "employee" },
+        { user_id: "4", username: "john", password: "pass", role: "employee" }];
+        const expectedResult = { user_id: "4", username: "john", role: "employee" };
+        let result = null
+        //Action
+        result = await userService.createUser(username, password);
+        //Assert
+        expect(result).toEqual(expectedResult);
+        expect(mockUserDB).toEqual(dbResult);
+        expect(userDao.getUserByUsername).toHaveBeenCalled();
+        expect(userDao.createUser).toHaveBeenCalled();
+    });
+});
+
+describe("Testing user logins via userService.getUserByUsername", () => {
+    beforeEach(() => {
+        rebuildDatabase();
+        userDao.getUserByUsername.mockClear();
+    });
+    test("Logging in with no username", async () => {
+        //Arrange
+        const username = "";
+        const password = "1234";
+
+        const expectedResult = null;
+        let result = null;
+        //Action
+        result = await userService.getUserByUsernamePassword(username, password);
+        //Assert
+        expect(result).toEqual(expectedResult);
+        expect(userDao.getUserByUsername).not.toHaveBeenCalled();
+    }); 
+    test("Logging in with no password", async () => {
+        //Arrange
+        const username = "alex";
+        const password = "";
+
+        const expectedResult = null;
+        let result = null;
+        //Action
+        result = await userService.getUserByUsernamePassword(username, password);
+        //Assert
+        expect(result).toEqual(expectedResult);
+        expect(userDao.getUserByUsername).not.toHaveBeenCalled();
+    });
+    test("Logging in with a bad password", async () => {
+        //Arrange
+        const username = "alex";
+        const password = "12345";
+
+        const expectedResult = null;
+        let result = null;
+        //Action
+        result = await userService.getUserByUsernamePassword(username, password);
+        //Assert
+        expect(result).toEqual(expectedResult);
+        expect(userDao.getUserByUsername).toHaveBeenCalled();
+    });
+    test("Logging in with no user in database", async () => {
+        //Arrange
+        const username = "john";
+        const password = "1234";
+
+        const expectedResult = null;
+        let result = null;
+        //Action
+        result = await userService.getUserByUsernamePassword(username, password);
+        //Assert
+        expect(result).toEqual(expectedResult);
+        expect(userDao.getUserByUsername).toHaveBeenCalled();
+    });
+    test("Logging in with a valid user", async () => {
         //Arrange
         const username = "alex";
         const password = "1234";
+
+        const expectedResult = { user_id: "1", username: "alex", role: "employee" };
+        let result = null;
         //Action
-
+        result = await userService.getUserByUsernamePassword(username, password);
         //Assert
-
-
-    })
-})
+        expect(result).toEqual(expectedResult);
+        expect(userDao.getUserByUsername).toHaveBeenCalled();
+    });
+});
